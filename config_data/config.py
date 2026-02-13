66454a7ce711a334759bdcdfe8ff95518c6a8acf
ben <2390188@mail.ru>
2026-02-13 17:00:45 +0300

    исправил пути под текущий докер

--- config_data/config.py
+++ config_data/config.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv, find_dotenv
 
 # env_path = './outside/.env'
-env_path = '/home/ben/PycharmProjects/maduro/outside/.env'
+env_path = '/home/ben/PycharmProjects/madurodocker/outside/.env'
 
 if not os.path.exists(env_path):
     exit("Переменные окружения не загружены, т.к. отсутствует файл .env по указанному пути")
